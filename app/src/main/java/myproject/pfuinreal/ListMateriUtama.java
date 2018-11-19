3a438b53202aeaaf912a7e03819a2ffbe3726e9f
unknown <arifglory46@gmail.com>
2018-11-20 00:07:05 +0700

    20nov18

---
+++ app/src/main/java/myproject/pfuinreal/ListMateriUtama.java
@@ -0,0 +1,13 @@
+package myproject.pfuinreal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListMateriUtama extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_materi_utama);
+    }
+}
---
+++ app/src/main/java/myproject/pfuinreal/MainActivity.java
@@ -0,0 +1,26 @@
+package myproject.pfuinreal;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnLogin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnLogin = (Button) findViewById(R.id.loginBtn);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
